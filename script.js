7c296d0545c585665230f18ac61ad978fd98befc
dariakondrateva <mikekondratyev@gmail.com>
2023-03-09 21:02:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// console.log ('Hello world');
+
+// let num = 4;
+// let name = 'Mikhail korov doil';
+// const pi = 3.14;
+// console.log(name);
+
+// let result = prompt('Enter num',0);
+// console.log(result);
+
+// типы данных
+// Number
+// String
+// Boolean
+// undefined
+// BigInt
+// Symbol
+// null
+// Object
+// let num1=123;
+// let num2=12.2333;
+// // NaN = Not a Number, infinity , - infinity
+
+// console.log(2+3);
+// console.log(2**3);
+// console.log(5%2);
+
+// String
+
+// let name = 'ddd';
+// console.log("im " + num1);
+// let r = 12;
+// let g = 34;
+// let b = 14;
+// console.log("RGB("+r+", " + g+", " + b+ ")");
+// console.log(`RGB(${r}, ${g}, ${b})`);
+
+
+// // Type of
+// let r = 12;
+// console.log(typeof r);
+
+// let age = prompt ('How old r u?');
+// console.log(typeof age); // string  всегда выводит
+
+// let num = prompt ('Num?');
+//  let pow = Number (num) ** 2; // можно делать + 
+//  console.log(pow);
+
+//  let num2 =  123;
+//  console.log (String(num2));
+// console.log ('3'+3); // concat
+// console.log (+'3'+3); // неявное преобразование 3 в число и выводит 6
+// console.log ('9'*3); // он сам преобразовывает 9 в число и умножает
+// console.log ('9 apples '*3); // тут уже Nan так как символы в строке
+
+let num1 = Number (prompt ('1'));
+let num2 = Number (prompt ('2'));
+let sum = num1+num2;
+console.log (sum);
+
